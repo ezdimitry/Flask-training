a36f80160baa7edebf64ab7fb7e46986205e7e7d
ezdimitry <dimoncl04@mail.ru>
2024-07-16 12:39:16 +0300

    add authorized, profile, errorhandler

--- flaskTemplates.py
+++ flaskTemplates.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
 
 app = Flask(__name__)
 
@@ -41,5 +41,30 @@ def contact():
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
 
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"Профиль пользователя: {username}"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "Dima" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
